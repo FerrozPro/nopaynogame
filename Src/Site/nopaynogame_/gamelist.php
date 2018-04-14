ca526df8d25d95dacb6c6a3719037ed509f17fb3
FerrozPro <ferrotti.elia@gmail.com>
2018-04-14 15:28:20 +0200

    update ricerca + fix stelle con query a db

--- Src/Site/nopaynogame_/gamelist.php
+++ Src/Site/nopaynogame_/gamelist.php
@@ -8,7 +8,12 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<style>
+		.checked {
+				color: orange;
+		}
+		</style>
     <title>NoPayNoGame</title>
   </head>
   
@@ -49,6 +54,7 @@
 						$cod_s = $_POST['cod_saldo'];
 						$cod_n = $_POST['cod_novita'];
 						$cod_t = $_POST['cod_testo'];
+						$cod_p = $_POST['cod_prezzo'];
 
 						$lista_giochi = mysql_query("
 						select distinct g.* 
@@ -60,46 +66,55 @@
 							and g.flag_sale like '%".$cod_s."%'
 							and gg.cod_genre like '%".$cod_g."%'
 							and g.cod_console like '%".$cod_c."%'
+							and g.price_on_sale between $cod_p and $cod_p+15
 						");
 
 						echo"<h1>Risultati Ricerca</h1></div><div class='row'>";						
 						break;
 				}
 				
-			  while($gioco=mysql_fetch_row($lista_giochi)){
-				$cod_gioco = $gioco[0];
-				$nome_gioco = $gioco[1];
-				$prezzo_gioco = $gioco[2];
-				$prezzo_saldo = $gioco[4];
-				$novita = $gioco[6];
-				$img = $gioco[7];
-				$console = mysql_fetch_row(mysql_query("select desc_console from my_nopaynogame.DOM_CONSOLE where cod_console = '$gioco[3]'"));
-				
-				
-				echo"<div class='col-lg-3 col-md-4 col-sm-6'>";
-				  echo"<div class='card h-100'>";
-					echo"<a href='game.php?game=".$cod_gioco."'><img class='card-img-top img-fluid' src='".$img."' alt=''></a>";
-					echo"<div class='card-body'>";
-					  echo"<h4 class='card-title'>";
-						echo"<a href='game.php?game=".$cod_gioco."'>".$nome_gioco."</a>";
-					  echo"</h4>";
-					  if($prezzo_saldo < $prezzo_gioco){
-						  echo"<h5>€<del>".$prezzo_gioco."</del> -->".$prezzo_saldo."</h5>";
-						}else{
-						  echo"<h5>€".$prezzo_gioco."</h5>";
-						}
-					  echo"<p class='card-text'>".$console[0]."</p>";
+				while($gioco=mysql_fetch_row($lista_giochi)){
+					$cod_gioco = $gioco[0];
+					$nome_gioco = $gioco[1];
+					$prezzo_gioco = $gioco[2];
+					$prezzo_saldo = $gioco[4];
+					$novita = $gioco[6];
+					$img = $gioco[7];
+					$console = mysql_fetch_row(mysql_query("select desc_console from my_nopaynogame.DOM_CONSOLE where cod_console = '$gioco[3]'"));
+					
+					echo"<div class='col-lg-3 col-md-4 col-sm-6'>";
+						echo"<div class='card h-100'>";
+							echo"<a href='game.php?game=".$cod_gioco."'><img class='card-img-top img-fluid' src='".$img."' alt=''></a>";
+							echo"<div class='card-body'>";
+								echo"<h4 class='card-title'>";
+								echo"<a href='game.php?game=".$cod_gioco."'>".$nome_gioco."</a>";
+								echo"</h4>";
+
+								$stars = mysql_fetch_row(mysql_query("select AVG(stars) from my_nopaynogame.REVIEW where cod_game = '$cod_gioco'"));
+								echo'<span class="fa fa-star'; if($stars[0] >= 1 ) { echo' checked'; } echo'"></span>';
+								echo'<span class="fa fa-star'; if($stars[0] >= 2 ) { echo' checked'; } echo'"></span>';
+								echo'<span class="fa fa-star'; if($stars[0] >= 3 ) { echo' checked'; } echo'"></span>';
+								echo'<span class="fa fa-star'; if($stars[0] >= 4 ) { echo' checked'; } echo'"></span>';
+								echo'<span class="fa fa-star'; if($stars[0] == 5 ) { echo' checked'; } echo'"></span>';
+
+								if($prezzo_saldo < $prezzo_gioco){
+									echo"<h5>€<del>".$prezzo_gioco."</del> -->".$prezzo_saldo."</h5>";
+								}else{
+									echo"<h5>€".$prezzo_gioco."</h5>";
+								}
+								echo"<p class='card-text'>".$console[0]."</p>";
+							echo"</div>";
+
+							echo"<div class='card-footer'>";
+							echo"<form method='POST' action='addtocart.php'>
+								<input type='hidden' name='cod_gioco' value=".$cod_gioco." />
+								<button type='submit' class='btn btn-block btn-warning'>Aggiungi al Carrello</button>
+							</form>";
+							echo"</div>";
+
+						echo"</div>";
 					echo"</div>";
-					echo"<div class='card-footer'>";
-					  echo"<small class='text-muted'>&#9733; &#9733; &#9733; &#9733; &#9734;</small>";
-					  echo"<form method='POST' action='addtocart.php'>
-							<input type='hidden' name='cod_gioco' value=".$cod_gioco." />
-							<button type='submit' class='btn btn-warning'>Aggiungi al Carrello</button>
-						</form>";
-					echo"</div>";
-				  echo"</div>";
-				echo"</div>";
-			  }
+					}
 				
 			?>
       </div>
--- Src/Site/nopaynogame_/index.php
+++ Src/Site/nopaynogame_/index.php
@@ -7,7 +7,12 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<style>
+		.checked {
+				color: orange;
+		}
+		</style>
     <title>NoPayNoGame</title>
   </head>
   <header>
@@ -58,7 +63,7 @@
           ?>
         </select>
         </div>
-        <div class='col-md-3'>
+        <div class='col-md-2'>
         <label for="cod_saldo">Saldo</label>
         <select id="cod_saldo" name='cod_saldo' class="form-control">
           <option selected></option>
@@ -66,12 +71,23 @@
           <option  value="N">NO</option>
         </select>
         </div>
-        <div class='col-md-3'>
+        <div class='col-md-2'>
         <label for="cod_novita">Novità</label>
         <select id="cod_novita" name='cod_novita' class="form-control">
           <option selected></option>
           <option  value="Y">SI</option>
           <option  value="N">NO</option>
+        </select>
+        </div>
+        <div class='col-md-2'>
+        <label for="cod_prezzo">Prezzo</label>
+        <select id="cod_prezzo" name='cod_prezzo' class="form-control">
+          <option selected></option>
+          <option  value="0">0 - 15 €</option>
+          <option  value="15">15 - 30 €</option>
+          <option  value="30">30 - 45 €</option>
+          <option  value="45">45 - 60 €</option>
+          <option  value="60">60 - 75 €</option>
         </select>
         </div>
       </div>
@@ -112,47 +128,53 @@
           <div class="row">
 			<?php 
 			
-			  $lista_giochi_home = mysql_query("
+			  $lista_giochi= mysql_query("
 			  select * 
 			  from my_nopaynogame.GAMES 
 			  where flag_sale = 'Y' or flag_news = 'Y' ");
 			  
-			  while($gioco=mysql_fetch_row($lista_giochi_home)){
-				$cod_gioco = $gioco[0];
-				$nome_gioco = $gioco[1];
-				$prezzo_gioco = $gioco[2];
-				$prezzo_saldo = $gioco[4];
-				$novità = $gioco[6];
-				$img = $gioco[7];
-				$console = mysql_fetch_row(mysql_query("select desc_console from my_nopaynogame.DOM_CONSOLE where cod_console = '$gioco[3]'"));
-				
-				
-				echo"<div class='col-lg-3 col-md-4 col-sm-6'>";
-				  echo"<div class='card h-100'>";
-					echo"<a href='game.php'><img class='card-img-top img-fluid' src='".$img."' alt=''></a>";
-					echo"<div class='card-body'>";
-					  echo"<h4 class='card-title'>";
-						echo"<a href='game.php'>".$nome_gioco."</a>";
-					  echo"</h4>";
-					  if($prezzo_saldo < $prezzo_gioco){
-						  echo"<h5>€<del>".$prezzo_gioco."</del> -->".$prezzo_saldo."</h5>";
-						}else{
-						  echo"<h5>€".$prezzo_gioco."</h5>";
-						}
-					  echo"<p class='card-text'>".$console[0]."</p>";
+			  while($gioco=mysql_fetch_row($lista_giochi)){
+					$cod_gioco = $gioco[0];
+					$nome_gioco = $gioco[1];
+					$prezzo_gioco = $gioco[2];
+					$prezzo_saldo = $gioco[4];
+					$novita = $gioco[6];
+					$img = $gioco[7];
+					$console = mysql_fetch_row(mysql_query("select desc_console from my_nopaynogame.DOM_CONSOLE where cod_console = '$gioco[3]'"));
+					
+					echo"<div class='col-lg-3 col-md-4 col-sm-6'>";
+						echo"<div class='card h-100'>";
+							echo"<a href='game.php?game=".$cod_gioco."'><img class='card-img-top img-fluid' src='".$img."' alt=''></a>";
+							echo"<div class='card-body'>";
+								echo"<h4 class='card-title'>";
+								echo"<a href='game.php?game=".$cod_gioco."'>".$nome_gioco."</a>";
+								echo"</h4>";
+
+								$stars = mysql_fetch_row(mysql_query("select AVG(stars) from my_nopaynogame.REVIEW where cod_game = '$cod_gioco'"));
+								echo'<span class="fa fa-star'; if($stars[0] >= 1 ) { echo' checked'; } echo'"></span>';
+								echo'<span class="fa fa-star'; if($stars[0] >= 2 ) { echo' checked'; } echo'"></span>';
+								echo'<span class="fa fa-star'; if($stars[0] >= 3 ) { echo' checked'; } echo'"></span>';
+								echo'<span class="fa fa-star'; if($stars[0] >= 4 ) { echo' checked'; } echo'"></span>';
+								echo'<span class="fa fa-star'; if($stars[0] == 5 ) { echo' checked'; } echo'"></span>';
+
+								if($prezzo_saldo < $prezzo_gioco){
+									echo"<h5>€<del>".$prezzo_gioco."</del> -->".$prezzo_saldo."</h5>";
+								}else{
+									echo"<h5>€".$prezzo_gioco."</h5>";
+								}
+								echo"<p class='card-text'>".$console[0]."</p>";
+							echo"</div>";
+
+							echo"<div class='card-footer'>";
+							echo"<form method='POST' action='addtocart.php'>
+								<input type='hidden' name='cod_gioco' value=".$cod_gioco." />
+								<button type='submit' class='btn btn-block btn-warning'>Aggiungi al Carrello</button>
+							</form>";
+							echo"</div>";
+
+						echo"</div>";
 					echo"</div>";
-					echo"<div class='card-footer'>";
-					  echo"<small class='text-muted'>&#9733; &#9733; &#9733; &#9733; &#9734;</small>";
-					  
-						echo"<form method='POST' action='addtocart.php'>
-							<input type='hidden' name='cod_gioco' value=".$cod_gioco." />
-							<button type='submit' class='btn btn-warning'>Aggiungi al Carrello</button>
-						</form>";
-					  					  
-					echo"</div>";
-				  echo"</div>";
-				echo"</div>";
-			  }
+					}
 				
 			?>
 
