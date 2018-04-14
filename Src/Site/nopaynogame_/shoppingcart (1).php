87dde38c14d09331addca7dc93c5316290d94984
alexiscatmiao <alessiadicampi@yahoo.it>
2018-04-14 22:07:29 +0200

    Add files via upload
    Modificata pagina principale dello shopping, ora se due prodotti (o piu) sono uguali aumenta la quantita

---
+++ Src/Site/nopaynogame_/shoppingcart (1).php
@@ -0,0 +1,475 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+session_start();
+require 'connection.php';
+$first=1;
+		
+		if(isset($_POST['buttoncheck'])){
+			$step2=1;
+			$back=0;
+		}else if(isset($_POST['buttonaddress'])){
+			$step3=1;
+			$step2=0;
+			$step5=0;
+			$step4=0;
+			$back=0;
+		}else if(isset($_POST['buttonpay'])){
+			$step4=1;
+			$step2=0;
+			$step3=0;
+			$step5=0;
+			$back=0;
+		}else if(isset($_POST['confirm'])){
+			$step5=1;
+			$step4=0;
+			$step2=0;
+			$step3=0;
+			$back=0;
+			
+		}else if(isset($_POST['back'])){
+			$back=1;
+		}
+		
+		
+$utente=$_SESSION['user'];
+$query ="SELECT * FROM USERS WHERE EMAIL='$utente'";
+$ris = ($conn->query($query));
+foreach($ris as $riga){
+  $surname = $riga ['SURNAME'];
+  $name = $riga ['NAME'];
+  $email= $riga ['EMAIL'];
+  $username= $riga ['USERNAME'];
+  $address= $riga ['ADDRESS'];
+  $phone= $riga ['PHONE'];
+  }
+ 
+$max=count($_SESSION['carrello']); 
+$i=0;
+//while($max==0){ 
+		$gioco=$_SESSION['carrello'][$i];
+		//echo $gioco;
+		$query ="SELECT * FROM GAMES WHERE COD_GAME='$gioco'";
+		$ris = ($conn->query($query));  
+		foreach($ris as $riga){
+			$title=$riga['TITLE'];
+		}
+		$i++;
+		//$max--;
+//}
+  
+?>
+  
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>NoPayNoGame</title>
+  </head>
+  
+  <header>
+  <?php	include 'header.php'; ?>
+  </header>
+
+  <body>
+
+    <!-- Navigation -->
+   
+    <!-- Page Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h1 class="mt-5">Shopping cart</h1>
+          <p class="lead">Hi <?php if(!isset($_SESSION['user'])){ echo "Utente Sconosciuto"; } else {echo $name;}?> this is your shopping cart</p>
+       <!--Tabella prodotti-->
+	   
+	   <?php if(($step2==0 && $step3==0 && $step4==0 & $step5==0) || $back==1){ ?>
+	    <div class="progress">
+  <div class="progress-bar progress-bar-striped" style="width:20%"></div>
+</div>
+	   
+		<table id="cart" class="table table-hover table-condensed">
+    				<?php if(!isset($_SESSION['carrello'])){?>
+					<p> Il tuo carrello è vuoto! </p>
+					<?php }else{ ?>
+					<thead>
+						<tr>
+							<th style="width:50%">Product</th>
+							<th style="width:10%">Price</th>
+							<th style="width:8%">Quantity</th>
+							<th style="width:22%" class="text-center">Subtotal</th>
+							<th style="width:10%"></th>
+						</tr>
+					</thead>
+					
+					<?php
+					$max=count($_SESSION['carrello']);
+					
+					for($i=0;$i<$max;$i++){
+						$array[$i]=1;
+					}
+					
+					for($i=0;$i<$max;$i++){
+						for($j=1;$j<$max;$j++){
+							
+							if($_SESSION['carrello'][$i] == $_SESSION['carrello'][$j] )
+								$array[$i]=$array[$i]+1;
+							else $array[$i]=$array[$i]+0;
+						}
+					}
+					
+					
+					
+					$i=0;
+					$result = array_unique($_SESSION['carrello']);
+					$max=count($result);
+					while($max!=0){ 
+								
+							$gioco=$result[$i];
+							$query ="SELECT *,count(*) AS quantita_aggiunta FROM GAMES WHERE COD_GAME='$gioco'";
+							
+							$ris = ($conn->query($query));  
+							foreach($ris as $riga){
+								
+							
+					//}?>
+					<tbody>
+						<tr>
+							<td data-th="Product">
+								<div class="row">
+									<div class="col-sm-2 hidden-xs"><img src=<?php echo $riga['IMAGE']; ?> alt=<?php echo $riga['TITLE']; ?> class="img-responsive" style="width:200px; heigth:200px;"/></div>
+									<div class="col-sm-10">
+										<h4 class="nomargin"><?php echo $riga['TITLE']; ?></h4>
+										<p><?php echo $riga['DESCRIPTION']; ?></p>
+									</div>
+								</div>
+							</td>
+							<td data-th="Price"><?php echo $riga['PRICE']; ?></td>
+							<td data-th="Quantity">
+								<input type="number" class="form-control text-center" value=<?php echo $array[$i]; ?>>
+							</td>
+							<td data-th="Subtotal" class="text-center"><?php $riga['PRICE']; ?></td>
+							<td class="actions" data-th="">
+								<button class="btn btn-info btn-sm"><i class="material-icons">autorenew</i></button>
+								<button class="btn btn-danger btn-sm"><i class="material-icons">delete_forever</i></button>								
+							</td>
+						</tr>
+					</tbody>
+					
+					<?php }
+						$i++;
+						$max--; 
+						$total=$total+$riga['PRICE'];
+					}?>
+					<tfoot>
+						<tr class="visible-xs">
+							<td class="text-center"><strong></strong></td>
+						</tr>
+						<tr>
+							<td><a href="http://nopaynogame.altervista.org/nopaynogame_/gamelist.php?cp=cat&tipo_ricerca=catalogo" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+							<td colspan="2" class="hidden-xs"></td>
+							<td class="hidden-xs text-center"><strong><?php echo $total; ?></strong></td>
+							<?php if(!isset($_SESSION['user'])) echo " <td><button type='submit' name='buttoncheck' class='btn btn-sucess btn-md' data-toggle='modal' data-target='#myModalregistrazione'>Checkout con registrazione</button></a></td>" ;
+							else echo" <form method=post><td><button type='submit' class='btn btn-success btn-md' name='buttoncheck' >Checkout</button></td></form>"; ?>
+						</tr>
+					</tfoot><?php } ?>
+				</table>
+	   <?php }else if($step2==1 && $step3==0 && $step4==0 & $step5==0){ ?>
+	   <div class="progress">
+		<div class="progress-bar progress-bar-striped" style="width:40%"></div>
+		</div>
+	    Il tuo indirizzo:
+	     <table class="table">
+			  <thead>
+				<tr>
+				  <th scope="col">Nome</th>
+				  <th scope="col">Cognome</th>
+				  <th scope="col">Indirizzo di spedizione</th>
+				  <th scope="col"></th>
+				</tr>
+			  </thead>
+			  <tbody>
+				<tr>
+				  <th scope="row"><?php echo $name; ?></th>
+				  <td><?php echo $surname; ?></td>
+				  <td><?php echo $address; ?></td>
+				  <td> <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Modifica</button></td>
+				</tr>
+				
+			  </tbody>
+			</table>
+			<form method='post'>
+			<button type='submit' class='btn btn-warning btn-md' name='back'>Torna indietro</button>
+			<button type='submit' class='btn btn-success btn-md' name='buttonaddress'>Procedi al pagamento</button>
+			
+			</form>
+	   
+	   <?php }else if($step2==0 && $step3==1 && $step4==0 & $step5==0){ ?>
+	   <div class="progress">
+		<div class="progress-bar progress-bar-striped" style="width:60%"></div>
+		</div>
+	      Metodo di pagamento:
+	     <table class="table">
+			  <thead>
+				<tr>
+				  <th scope="col">Metodo</th>
+				  <th scope="col">First</th>
+				  
+				</tr>
+			  </thead>
+			  <tbody>
+				<tr>
+				  <th scope="row">1</th>
+				  <td><label>
+					  <input type="radio" name="fb" value="small" />
+					  <img src="fb1.jpg">
+					</label></td>
+				  
+				</tr>
+				<tr>
+				  <th scope="row">2</th>
+				 <td><label>
+					  <input type="radio" name="fb" value="small" />
+					  <img src="fb1.jpg">
+					</label></td>
+				  
+				</tr>
+				<tr>
+				  <th scope="row">3</th>
+				  <td><label>
+					  <input type="radio" name="fb" value="small" />
+					  <img src="fb1.jpg">
+					</label></td>
+				  
+				</tr>
+			  </tbody>
+			</table>
+			<form method='post'>
+               <button type='submit' class='btn btn-warning btn-md' name='buttoncheck'>Torna indietro</button>
+				<button type='submit' class='btn btn-success btn-md' name='buttonpay'>Ultimo step!</button>
+				
+			</form>
+	   
+	   <?php }else if($step2==0 && $step3==0 && $step4==1 & $step5==0){ ?>
+	   <div class="progress">
+		<div class="progress-bar progress-bar-striped" style="width:100%"></div>
+	   </div>
+	    
+	      Riepilogo:
+	     <table class="table">
+			  <thead>
+				<tr>
+				  <th scope="col">#</th>
+				  <th scope="col">First</th>
+				  <th scope="col">Last</th>
+				  <th scope="col">Handle</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				<tr>
+				  <th scope="row">1</th>
+				  <td>Mark</td>
+				  <td>Otto</td>
+				  <td>@mdo</td>
+				</tr>
+				<tr>
+				  <th scope="row">2</th>
+				  <td>Jacob</td>
+				  <td>Thornton</td>
+				  <td>@fat</td>
+				</tr>
+				<tr>
+				  <th scope="row">3</th>
+				  <td>Larry</td>
+				  <td>the Bird</td>
+				  <td>@twitter</td>
+				</tr>
+			  </tbody>
+			</table>
+			
+			<form method='post'>
+			<button type='submit' class='btn btn-warning btn-md' name='buttonaddress'>Torna indietro</button>
+			<button type='submit' class='btn btn-success btn-md' name='confirm'>Invia ordine</button>
+			
+			</form>
+		    
+		  </div>
+		  
+	   
+	   <?php }else if($step2==0 && $step3==0 && $step4==0 & $step5==1){ ?>
+	     <h2>Ordine ricevuto!</h2>
+		 <p>Id ordine: $idordine</p>
+		 <p>Saldo punti: $punti </p>
+	      <img src="img/check.png"></img>
+		 <p> Riceverai il tuo ordine tra 48h</p>
+	   
+	   <?php }?>
+				
+	
+
+       
+			
+			<!-- Modal senza accesso -->
+			<div id="myModalregistrazione" class="modal fade" role="dialog">
+			  <div class="modal-dialog">
+
+				<!-- Modal content-->
+				<div class="modal-content">
+				  <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">OPS! Abbiamo notato che non hai effettuato l'accesso..</h4>
+					
+				  </div>
+								
+					
+					<div id="accordion">
+						  <div class="card">
+							<div class="card-header" id="headingOne">
+							  <h5 class="mb-0">
+								<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+								  Accedi
+								</button>
+							  </h5>
+							</div>
+
+							<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+							  <div class="card-body">
+							  <form>
+								  <div class="form-group">
+									<label for="exampleInputEmail1">Email address</label>
+									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+									<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+								  </div>
+								  <div class="form-group">
+									<label for="exampleInputPassword1">Password</label>
+									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+								  </div>
+								  
+								  <button type="submit" class="btn btn-primary">Submit</button>
+								</form>
+							  </div>
+							</div>
+						  </div>
+						  <div class="card">
+							<div class="card-header" id="headingTwo">
+							  <h5 class="mb-0">
+								<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								  Sei nuovo? Registrati!
+								</button>
+							  </h5>
+							</div>
+							<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+							  <div class="card-body">
+							  <form>
+								  <div class="form-row">
+									<div class="form-group col-md-6">
+									  <label for="inputEmail4">Email</label>
+									  <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+									</div>
+									<div class="form-group col-md-6">
+									  <label for="inputPassword4">Password</label>
+									  <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+									</div>
+								  </div>
+								  <div class="form-group">
+									<label for="inputAddress">Username</label>
+									<input type="text" class="form-control" id="inputUsername" placeholder="@Jonny">
+								  </div>
+								  <div class="form-group">
+									<label for="inputAddress2">Address</label>
+									<input type="text" class="form-control" id="inputAddress" placeholder="Apartment, studio, or floor">
+								  </div>
+								  <div class="form-row">
+									<div class="form-group col-md-6">
+									  <label for="inputCity">City</label>
+									  <input type="text" class="form-control" id="inputCity">
+									</div>
+									<div class="form-group col-md-4">
+									  <label for="inputState">State</label>
+									  <select id="inputState" class="form-control">
+										<option selected>Choose...</option>
+										<option>...</option>
+									  </select>
+									</div>
+									<div class="form-group col-md-2">
+									  <label for="inputZip">Zip</label>
+									  <input type="text" class="form-control" id="inputZip">
+									</div>
+								  </div>
+								  <button type="submit" class="btn btn-primary">Sign in</button>
+								</form>
+							  </div>
+							</div>
+						  </div>
+						 
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						</div>
+				</div>
+
+			  </div>
+			</div>
+
+				
+        </div>
+      </div>
+    </div>
+
+	
+	  <!--MODAL MODIFICA DATI INDIRIZZO -->
+
+
+			<!-- The Modal -->
+			<div class="modal fade" id="myModal">
+			  <div class="modal-dialog">
+				<div class="modal-content">
+
+				  <!-- Modal Header -->
+				  <div class="modal-header">
+					<h4 class="modal-title">Modal Heading</h4>
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+				  </div>
+
+				  <!-- Modal body -->
+				  <div class="modal-body">
+					<label >Name:</label>
+					<input type="text" class="form-control" value= <?php echo $name; ?>>
+					<label>Surname:</label>
+					<input type="text" class="form-control"  value= <?php echo $surname; ?>>
+					<label >Indirizzo:</label>
+					<input type="text" class="form-control"  value= <?php echo $address; ?>>
+					
+					
+					
+					
+				  </div>
+
+				  <!-- Modal footer -->
+				  <div class="modal-footer">
+					<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+				  </div>
+
+				</div>
+			  </div>
+			</div>
+			
+			
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+
+</html>
+
+
+
