b2780f9e17346b971f1062e9dd201ba2ec66c954
FerrozPro <ferrotti.elia@gmail.com>
2018-04-20 22:44:34 +0200

    fix fab

--- Src/Site/nopaynogame_/game.php
+++ Src/Site/nopaynogame_/game.php
@@ -147,9 +147,13 @@
       <div class="row">
         <div class="col-md-12">
 
+
+
+<div class="list-group">
+  
          <?php
 		 $query_review = "SELECT * FROM REVIEW WHERE COD_GAME = '$codice_gioco'";
-						  
+		 
 		 $ris_review = ($conn->query($query_review));
 		 
 		 foreach ($ris_review as $riga) {
@@ -157,16 +161,26 @@
 			 $Id_user = $riga['ID_USER'];
 			 $Stelle = $riga['STARS'];
 			 $Commenti = $riga['COMMENT_TEXT'];
-
+			 
 			 echo $Id_review;
 			 echo $Id_user;
 			 echo $Stelle;
 			 echo $Commenti;
-
-		 }
-		 
-		 
-		 ?>
+			 echo'
+			 <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+			 <div class="d-flex w-100 justify-content-between">
+			 <h5 class="mb-1">List group item heading</h5>
+			 <small>3 days ago</small>
+			 </div>
+			 <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+			 <small>Donec id elit non mi porta.</small>
+			 </a>';
+			 
+			}
+			
+			
+			?>
+			</div>
 		 
         </div>
       </div>
