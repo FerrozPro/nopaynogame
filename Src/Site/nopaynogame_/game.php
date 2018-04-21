cc4cafa4d0f80efe17a4a09301982fff574755f2
FerrozPro <ferrotti.elia@gmail.com>
2018-04-21 20:12:56 +0200

    update fab

--- Src/Site/nopaynogame_/game.php
+++ Src/Site/nopaynogame_/game.php
@@ -13,6 +13,73 @@
 		.checked {
 				color: orange;
 		}
+		@media (min-width: 768px) {
+
+		/* show 3 items */
+		.carousel-inner .active,
+		.carousel-inner .active + .carousel-item,
+		.carousel-inner .active + .carousel-item + .carousel-item,
+		.carousel-inner .active + .carousel-item + .carousel-item + .carousel-item  {
+				display: block;
+		}
+
+		.carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left),
+		.carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left) + .carousel-item,
+		.carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left) + .carousel-item + .carousel-item,
+		.carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left) + .carousel-item + .carousel-item + .carousel-item {
+				transition: none;
+		}
+
+		.carousel-inner .carousel-item-next,
+		.carousel-inner .carousel-item-prev {
+			position: relative;
+			transform: translate3d(0, 0, 0);
+		}
+
+		.carousel-inner .active.carousel-item + .carousel-item + .carousel-item + .carousel-item + .carousel-item {
+				position: absolute;
+				top: 0;
+				right: -25%;
+				z-index: -1;
+				display: block;
+				visibility: visible;
+		}
+
+		/* left or forward direction */
+		.active.carousel-item-left + .carousel-item-next.carousel-item-left,
+		.carousel-item-next.carousel-item-left + .carousel-item,
+		.carousel-item-next.carousel-item-left + .carousel-item + .carousel-item,
+		.carousel-item-next.carousel-item-left + .carousel-item + .carousel-item + .carousel-item,
+		.carousel-item-next.carousel-item-left + .carousel-item + .carousel-item + .carousel-item + .carousel-item {
+				position: relative;
+				transform: translate3d(-100%, 0, 0);
+				visibility: visible;
+		}
+
+		/* farthest right hidden item must be abso position for animations */
+		.carousel-inner .carousel-item-prev.carousel-item-right {
+				position: absolute;
+				top: 0;
+				left: 0;
+				z-index: -1;
+				display: block;
+				visibility: visible;
+		}
+
+		/* right or prev direction */
+		.active.carousel-item-right + .carousel-item-prev.carousel-item-right,
+		.carousel-item-prev.carousel-item-right + .carousel-item,
+		.carousel-item-prev.carousel-item-right + .carousel-item + .carousel-item,
+		.carousel-item-prev.carousel-item-right + .carousel-item + .carousel-item + .carousel-item,
+		.carousel-item-prev.carousel-item-right + .carousel-item + .carousel-item + .carousel-item + .carousel-item {
+				position: relative;
+				transform: translate3d(100%, 0, 0);
+				visibility: visible;
+				display: block;
+				visibility: visible;
+		}
+
+}
 		</style>
 
     <title>NoPayNoGame</title>
@@ -52,35 +119,42 @@
 
     <div class="container">
       <h1><?php echo $Titolo?></h1>
-      <div class="row">
-        <div class="col-md-3">
+		<div class="row">
+			<div class="col-md-3">
 
+				<div>
+					<img class='img-fluid' src="<?php echo $Immagine?>" alt="Non va">
+				</div>
+				<div>
+					<?php
+					  $stars = mysql_fetch_row(mysql_query("select cast(AVG(stars)AS DECIMAL(10,1)) from my_nopaynogame.REVIEW where cod_game = '$Cod_gioco'"));
+											echo'<span class="fa fa-star'; if($stars[0] >= 1 ) { echo' checked'; } echo'"></span>';
+											echo'<span class="fa fa-star'; if($stars[0] >= 2 ) { echo' checked'; } echo'"></span>';
+											echo'<span class="fa fa-star'; if($stars[0] >= 3 ) { echo' checked'; } echo'"></span>';
+											echo'<span class="fa fa-star'; if($stars[0] >= 4 ) { echo' checked'; } echo'"></span>';
+											echo'<span class="fa fa-star'; if($stars[0] == 5 ) { echo' checked'; } echo'"></span><b> '.$stars[0].' </b>';
+					?>
+				</div>
 		
-		  <img class='img-fluid' src="<?php echo $Immagine?>" alt="Non va">
-		<?php
-		  $stars = mysql_fetch_row(mysql_query("select AVG(stars) from my_nopaynogame.REVIEW where cod_game = '$Cod_gioco'"));
-								echo'<span class="fa fa-star'; if($stars[0] >= 1 ) { echo' checked'; } echo'"></span>';
-								echo'<span class="fa fa-star'; if($stars[0] >= 2 ) { echo' checked'; } echo'"></span>';
-								echo'<span class="fa fa-star'; if($stars[0] >= 3 ) { echo' checked'; } echo'"></span>';
-								echo'<span class="fa fa-star'; if($stars[0] >= 4 ) { echo' checked'; } echo'"></span>';
-								echo'<span class="fa fa-star'; if($stars[0] == 5 ) { echo' checked'; } echo'"></span><b> '.$stars[0].' </b>';
-		?>
 		
-		
-		
-		  <?php if($Prezzo_saldo < $Prezzo){
-                  echo"<h5>€<del>".$Prezzo."</del> <i class='fa fa-chevron-right'></i> €".$Prezzo_saldo."</h5>";
-                  echo'<span class="badge badge-pill badge-danger">SALDO</span>';
-								}else{
-									echo"<h5>€".$Prezzo."</h5>";
-                }
-			?>
-	
+				<div>
+					<?php if($Prezzo_saldo < $Prezzo){
+							echo"<h5>€<del>".$Prezzo."</del> <i class='fa fa-chevron-right'></i> €".$Prezzo_saldo."</h5>";
+							echo'<span class="badge badge-pill badge-danger">SALDO</span>';
+							}
+							else {
+								echo"<h5>€".$Prezzo."</h5>";
+							}
+					?>
+				</div>
+	<div>
 		  <?php echo $FLAG_SALE;
 			if($Flag_novita == 'Y'){
 				echo'<span class="badge badge-pill badge-success">NUOVO!</span>';
 			}
 			?>
+			</div>
+			<div>
 		<?php
 			$query_console = "SELECT *
 							  FROM DOM_CONSOLE
@@ -93,7 +167,8 @@
 				  echo '<p>'.$Console_desc.'</p>';
 			 }
 		?>
-			
+		</div>
+			<div>
 			<?php
 			echo"<form method='POST' action='addtocart.php'>
                 <input type='hidden' name='cod_gioco' value=".$Cod_gioco." />";
@@ -106,9 +181,10 @@
                 } echo"
 							</form>";
 			?>
+			</div>
 			
 			<br>
-			
+			<div>
 			<?php
 	$query_genre = "SELECT dg.DESC_GENRE 
 			   FROM GAME_GENRE gg, DOM_GENRE dg
@@ -122,7 +198,7 @@
 		}
 	
 			?>
-			
+			</div>
 
 
 			
@@ -147,8 +223,12 @@
       <div class="row">
         <div class="col-md-12">
 
+     <br>
+	 
+	 <h5> Recensioni </h5>
 
-
+	 <br>
+	 
 <div class="list-group">
   
          <?php
@@ -162,31 +242,126 @@
 			 $Stelle = $riga['STARS'];
 			 $Commenti = $riga['COMMENT_TEXT'];
 			 
-			 echo $Id_review;
-			 echo $Id_user;
-			 echo $Stelle;
-			 echo $Commenti;
-			 echo'
-			 <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-			 <div class="d-flex w-100 justify-content-between">
-			 <h5 class="mb-1">List group item heading</h5>
-			 <small>3 days ago</small>
-			 </div>
-			 <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-			 <small>Donec id elit non mi porta.</small>
-			 </a>';
 			 
+			 ?>
+			 
+			 <?php 
+			 
+			 $query_nomeutente = "SELECT * FROM USERS WHERE ID_USER = '$Id_user'";
+			 
+			  $ris_nomeutente = ($conn->query($query_nomeutente));
+			  
+			  foreach ($ris_nomeutente as $riga) {
+				  $Nome_utente = $riga['USERNAME'];
+			  }
+			 
+			 
+			 ?>
+			 
+			 <div class="list-group">
+  <a  class="list-group-item list-group-item-action flex-column align-items-start ">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1"><?php echo $Nome_utente ?></h5>
+      <small><?php  
+	  echo'<span class="fa fa-star'; if($Stelle[0] >= 1 ) { echo' checked'; } echo'"></span>';
+											echo'<span class="fa fa-star'; if($Stelle[0] >= 2 ) { echo' checked'; } echo'"></span>';
+											echo'<span class="fa fa-star'; if($Stelle[0] >= 3 ) { echo' checked'; } echo'"></span>';
+											echo'<span class="fa fa-star'; if($Stelle[0] >= 4 ) { echo' checked'; } echo'"></span>';
+											echo'<span class="fa fa-star'; if($Stelle[0] == 5 ) { echo' checked'; } echo'"></span>';
+	  
+	  
+	  
+	  ?></small>
+    </div>
+    <p class="mb-1"><?php echo $Commenti ?></p>
+
+  </a>
+  
+</div>
+
+			<?php 
 			}
-			
-			
 			?>
+			
+			<div class="form-group">
+			<label for="exampleTextarea">La tua recensione</label>
+			<textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+			</div>
+			
 			</div>
 		 
         </div>
       </div>
       <div class="row">
         <div class="col-md-12">
-        giochi consigliati?
+		
+		<?php 
+		$query_random_game = "SELECT * FROM GAMES ga, GAME_GENRE gg, DOM_GENRE dg WHERE dg.COD_GENRE = gg.COD_GENRE AND gg.COD_GAME = ga.COD_GAME ORDER BY RAND() LIMIT 3";
+		
+		$ris_random_game = ($conn->query($query_random_game));
+		
+		foreach ($ris_random_game as $riga) {
+			
+			$Immagine_gioco_random = $riga['IMAGE'];
+			$Titolo_gioco_random = $riga['TITLE'];
+			$Prezzo_gioco_random = $riga['PRICE'];
+		
+		
+		?>
+		<div class="card-group">
+	<div class="card">
+    <img class="card-img-top" src="<?php echo $Immagine_gioco_random?>" style='heigth:180px; width:100px; '>
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $Titolo_gioco_random ?></h5>
+      <p class="card-text"><?php echo $Prezzo_gioco_random ?></p>
+    </div>
+		</div>
+  
+		<?php 	
+		}
+		?>
+ 
+	</div>
+
+	<div class="container-fluid">
+    <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
+        <div class="carousel-inner row w-100 mx-auto" role="listbox">
+            <div class="carousel-item col-md-3 active">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400/000/fff?text=1" alt="slide 1">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=2" alt="slide 2">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=3" alt="slide 3">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=4" alt="slide 4">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=5" alt="slide 5">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=6" alt="slide 6">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=7" alt="slide 7">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=8" alt="slide 7">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+            <i class="fa fa-chevron-left fa-lg text-muted"></i>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
+            <i class="fa fa-chevron-right fa-lg text-muted"></i>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+ 
         </div>
       </div>
     </div>
@@ -194,6 +369,28 @@
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <?php include 'script.php'; ?>
+		<script js>
+		$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+var $e = $(e.relatedTarget);
+var idx = $e.index();
+var itemsPerSlide = 4;
+var totalItems = $('.carousel-item').length;
+
+if (idx >= totalItems-(itemsPerSlide-1)) {
+		var it = itemsPerSlide - (totalItems - idx);
+		for (var i=0; i<it; i++) {
+				// append slides to end
+				if (e.direction=="left") {
+						$('.carousel-item').eq(i).appendTo('.carousel-inner');
+				}
+				else {
+						$('.carousel-item').eq(0).appendTo('.carousel-inner');
+				}
+		}
+}
+});
+		</script>
   </body>
 </html>
 
